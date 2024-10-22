7cdcb5b19027d9cc9aea3dd09372c88774fe8420
wook <v109358@naver.com>
2024-10-22 17:04:37 +0900

    관욱 커밋_2024.10.22(오후)

--- src/main/java/com/fitmate/crew/service/CrewService.java
+++ src/main/java/com/fitmate/crew/service/CrewService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fitmate.crew.dao.CrewDAO;
+import com.fitmate.crew.dto.CrewSearchConditionDTO;
+import com.fitmate.crew.dto.CrewSearchListDTO;
 
 @Service
 public class CrewService {
@@ -27,39 +29,39 @@ public class CrewService {
 	}
 
 
-
-	public List<Map> crewList(Map<String, String> params) {
+	public List<CrewSearchListDTO> crewList(Map<String, String> params) {
 		
 		// 1. 받아온 데이터가공.
 		String searchFilter_ = params.get("searchFilter");      // searchFilter [1='크루이름' / 2='크루장닉네임']
 		String searchKeyword = params.get("searchKeyword");    // searchKeyword [검색키워드]
-		String placeFilter_ = params.get("placeFilter");        // placeFilter [region_idx, regions_idx를 분리]
+		String placeFilter_ = params.get("placeFilter");        // placeFilter [regions_idx]
 		String mbtiFilter_ = params.get("mbtiFilter");          // mbtiFilter [프로필-mbtir_idx]
 		
 		
+		CrewSearchConditionDTO searchDTO = new CrewSearchConditionDTO();
+		
 		int searchFilter = 0;
-		int region_idx = 0;
 		int regions_idx = 0;
 		int mbtiFilter = 0;
 		
 		if(searchFilter_ != null && !searchFilter_.equals("")) {
-			searchFilter = Integer.parseInt(searchFilter_); 
+			searchDTO.setSearchFilter(Integer.parseInt(searchFilter_.trim())); 
 		}
 		if(placeFilter_ != null && !placeFilter_.equals("")) {
-			 String[] place = placeFilter_.split("_"); region_idx =
-			 Integer.parseInt(place[0]); regions_idx = Integer.parseInt(place[1]);
+			searchDTO.setRegions_idx(Integer.parseInt(placeFilter_.trim()));
 		}
 		if(mbtiFilter_ != null && !mbtiFilter_.equals("")) {
-			mbtiFilter = Integer.parseInt(mbtiFilter_); 
+			searchDTO.setMbtiFilter(Integer.parseInt(mbtiFilter_.trim()));
 		}
-		
 		
 		// Limit & Offset 세팅
 		int limit = 6;
 		int offset = 0;
+		searchDTO.setLimit(limit);
+		searchDTO.setOffset(offset);
 		
 		
-		return crew_dao.crewList(searchFilter, searchKeyword, region_idx, regions_idx, mbtiFilter, limit, offset);
+		return crew_dao.crewList(searchDTO);
 		
 	} 
 
