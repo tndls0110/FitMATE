0f55d7ca1e9951715fd5f18c947ad5cd54a4635b
suin_laps <suin@email.com>
2024-10-22 11:28:41 +0900

    DTO 생성

--- src/main/java/com/fitmate/admin/dto/NoticeDTO.java
+++ src/main/java/com/fitmate/admin/dto/NoticeDTO.java
@@ -1,5 +1,45 @@
 package com.fitmate.admin.dto;
 
+import java.sql.Date;
+
 public class NoticeDTO {
+
+    private int notice_idx;
+    private String notice_cont;
+    private Date notice_regdate;
+    private int notice_show;
+    private int admin_idx;
+
+    public int getNotice_idx() {
+        return notice_idx;
+    }
+    public String getNotice_cont() {
+        return notice_cont;
+    }
+    public Date getNotice_regdate() {
+        return notice_regdate;
+    }
+    public int getNotice_show() {
+        return notice_show;
+    }
+    public int getAdmin_idx() {
+        return admin_idx;
+    }
+
+    public void setNotice_idx(int notice_idx) {
+        this.notice_idx = notice_idx;
+    }
+    public void setNotice_cont(String notice_cont) {
+        this.notice_cont = notice_cont;
+    }
+    public void setNotice_regdate(Date notice_regdate) {
+        this.notice_regdate = notice_regdate;
+    }
+    public void setNotice_show(int notice_show) {
+        this.notice_show = notice_show;
+    }
+    public void setAdmin_idx(int admin_idx) {
+        this.admin_idx = admin_idx;
+    }
 
 }
--- src/main/java/com/fitmate/admin/dto/RegReportDTO.java
+++ src/main/java/com/fitmate/admin/dto/RegReportDTO.java
@@ -2,4 +2,35 @@ package com.fitmate.admin.dto;
 
 public class RegReportDTO {
 
+    private int reportr_idx;
+    private String reportr_con;
+    private int reportr_show;
+    private int admin_idx;
+
+    public int getReportr_idx() {
+        return reportr_idx;
+    }
+    public String getReportr_con() {
+        return reportr_con;
+    }
+    public int getReportr_show() {
+        return reportr_show;
+    }
+    public int getAdmin_idx() {
+        return admin_idx;
+    }
+
+    public void setReportr_idx(int reportr_idx) {
+        this.reportr_idx = reportr_idx;
+    }
+    public void setReportr_con(String reportr_con) {
+        this.reportr_con = reportr_con;
+    }
+    public void setReportr_show(int reportr_show) {
+        this.reportr_show = reportr_show;
+    }
+    public void setAdmin_idx(int admin_idx) {
+        this.admin_idx = admin_idx;
+    }
+
 }
