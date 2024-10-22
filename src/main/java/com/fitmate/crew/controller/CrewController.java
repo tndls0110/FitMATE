e2105e719a52c9f3e77cdc7b15db2851a236e3cb
kyh <fucknaber0720@naver.com>
2024-10-22 09:26:54 +0900

    김용현입니다

--- src/main/java/com/fitmate/crew/controller/CrewController.java
+++ src/main/java/com/fitmate/crew/controller/CrewController.java
@@ -2,10 +2,21 @@ package com.fitmate.crew.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fitmate.crew.service.CrewService;
 
 @Controller
 public class CrewController {
 	Logger logger = LoggerFactory.getLogger(getClass());
-
+	
+	@Autowired CrewService crew_service;
+		
+	@RequestMapping(value="crew_create.go")
+	public String crew_create() {
+		
+		return "crew_create";
+	}
 }
