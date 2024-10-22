ebac66e947bcc04605d144cf359a906531aa0142
kang,kwanwook <kwanwook97@gmail.com>
2024-10-22 10:25:41 +0900

    kwanwook commit_2024.10.22

--- src/main/java/com/fitmate/crew/dao/CrewDAO.java
+++ src/main/java/com/fitmate/crew/dao/CrewDAO.java
@@ -1,5 +1,14 @@
 package com.fitmate.crew.dao;
 
+import java.util.List;
+import java.util.Map;
+
 public interface CrewDAO {
+
+	List<Map> placeFilter();
+
+	List<Map> mbtiFilter();
+
+	List<Map> crewList(int searchFilter, String searchKeyword, int region_idx, int regions_idx, int mbtiFilter,int limit, int offset);
 
 }
