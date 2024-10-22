c81557488b5e3c0686bc3d44b89ad8981be640d5
류경민 <rrarro000115@gmail.com>
2024-10-22 15:38:20 +0900

    ScheduleController 주소 수정 -> main으로 바로 갈 수 있게....

--- src/main/java/com/fitmate/schedule/controller/ScheduleController.java
+++ src/main/java/com/fitmate/schedule/controller/ScheduleController.java
@@ -3,9 +3,16 @@ package com.fitmate.schedule.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ScheduleController {
 	Logger logger = LoggerFactory.getLogger(getClass());
-
+	
+	@RequestMapping (value = "/")
+	public String main() {
+		return "schedule";
+	}
+	
+	
 }
