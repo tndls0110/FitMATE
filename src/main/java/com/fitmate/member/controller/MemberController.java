56e5113e5782fd259348e83909c1dc2893dda1c2
황수인 <tndls@email.net>
2024-10-22 09:05:40 +0900

    배경 이미지 추가, 로그인 작성중

--- src/main/java/com/fitmate/member/controller/MemberController.java
+++ src/main/java/com/fitmate/member/controller/MemberController.java
@@ -2,10 +2,23 @@ package com.fitmate.member.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fitmate.member.service.MemberService;
 
 @Controller
 public class MemberController {
 	Logger logger = LoggerFactory.getLogger(getClass());
-
+	@Autowired MemberService member_service;
+	
+	String page = "";
+	
+	@RequestMapping (value = "/member_login.go")
+	public String login() {
+		
+		return "member_login";
+	}
+	
 }
