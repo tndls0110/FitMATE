b3e97d659baabb112a710b30df42cbcb12ce456b
류경민 <rrarro000115@gmail.com>
2024-10-24 15:39:28 +0900

    다음 페이지 불러오는 로직 작성

--- src/main/java/com/fitmate/mbti/controller/MbtiController.java
+++ src/main/java/com/fitmate/mbti/controller/MbtiController.java
@@ -70,4 +70,12 @@ public class MbtiController {
 		Map<String,Object> data = m_service.getTypeScore(Oidx);
 		return data;
 	};
+
+	@GetMapping (value = "/nextPageIdx.ajax")
+	@ResponseBody
+	public Map<String, Object> nextPageIdx(int lastQIdx){
+	Map<String,Object> result = new HashMap<>();
+	result.put("idx",m_service.nextPageIdx(lastQIdx));
+	return result;
+	};
 }
