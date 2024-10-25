36092db85baae72a245d64f47836b70878d336db
kyh <fucknaber0720@naver.com>
2024-10-25 15:11:41 +0900

    사진 한줄 신고

--- src/main/java/com/fitmate/crew/controller/CrewPageController.java
+++ src/main/java/com/fitmate/crew/controller/CrewPageController.java
@@ -1,5 +1,6 @@
 package com.fitmate.crew.controller;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,9 +10,12 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,23 +47,31 @@ public class CrewPageController {
 	
 	// 신고 페이지 이동 test code
 	@RequestMapping(value="/crew_report.go")
-	public String crew_report() {
-
+	public String crew_report(Map<String,String> param,Model model) {
+		
+		// 보드idx가 있다면
+		if(param.get("board_idx")!=null) {
+			// 신고 board_type 은 게시물
+			model.addAttribute("board_type", "1");
+		}
+		else {
+			// 신고 board_type 은 댓글 // 댓글만 신고가능 대댓글신고 불가능
+			model.addAttribute("board_type", "2");
+		}
+		
 		return "crew_report";
 	}
 	
 	// 신고사유 리스트 불러오는 ajax
 	@GetMapping(value="/crew_report.ajax")
 	@ResponseBody
-	public Map<String,Object> report_list(){
+	public List<RegReportDTO> report_list(){
+	
+		List<RegReportDTO> report = crewpage_service.report_list();
+		logger.info("reportlist = {}",report);
 		
-		Map<String,Object> result = new HashMap<String, Object>();
 		
-		List<ReportDTO> reportlist = crewpage_service.report_list();
-		logger.info("reportlist = {}",reportlist);
-		result.put("reportlist", reportlist);
-		
-		return result;
+		return report;
 	}
 	// 신고하기
 	@RequestMapping(value="/crew_report.do")
@@ -71,7 +83,7 @@ public class CrewPageController {
 	/*
 	// 공지사항 페이지로 이동하기
 	@RequestMapping(value="/crew_page_notice.go")
-	public String crew_page_notice(@RequestParam Map<String,String> params,HttpSession session, Model model) {
+	public String crew_page_notice(String board_idx,HttpSession session, Model model) {
 	
 	//	logger.info("공지사항페이지 이동 {}", params);
 	//	model.addAttribute("crew_idx", "6");
@@ -105,12 +117,12 @@ public class CrewPageController {
 	
 	// 공지사항 작성하기
 	@RequestMapping(value="/crew_page_notice.do",method =RequestMethod.POST)
-	public String crew_notice_write(@RequestParam String subject,@RequestParam String board_id,@RequestParam int crew_idx) {
+	public String crew_notice_write(@RequestParam String content,@RequestParam String board_id,@RequestParam int crew_idx) {
 		// subject = 공지사항 내용이자 제목, board_id = 작성자, crew_idx = 크루 식별위한 변수 + board_idx 와 crew_idx 합쳐줄때(식별하기위해) 필요
 		
-		logger.info("입력값 {} ",subject);
+		logger.info("입력값 {} ",content);
 		
-		crewpage_service.crew_notice_write(subject,board_id,crew_idx);
+		crewpage_service.crew_notice_write(content,board_id,crew_idx);
 		
 		return "redirect:/crew_page_notice.go";
 	}
@@ -199,11 +211,12 @@ public class CrewPageController {
 			// 문자열인 크루idx를 int로 형변환
 			int crew_idx = Integer.parseInt(crewidx);
 			List<CrewBoardDTO> oneboardlist = crewpage_service.crew_oneboard_list(crew_idx);
+			// 닉네임가져오려면 List<CrewBoardDTO> 크기만큼 for문을 실행하고 CrewBoardDTO에서 board_id를 꺼내고 그 값을 회원 테이블에서 닉네임 가져오고 board_id와 닉네임을 바꾼다
 			
 			//크루장 확인  크루idx로 크루id 가져와서 조회하기
 			String master = params.get("board_id"); // 나중에 고칠 코드
 			
-			session.setAttribute("sessionId","member01"); // 세션아이디에 임시로 보드id 집어넣음 로그인기능 업데이트시 나중에 없애줄 코드?
+			// session.setAttribute("sessionId","member01"); // 세션아이디에 임시로 보드id 집어넣음 로그인기능 업데이트시 나중에 없애줄 코드?
 			
 			
 			logger.info("oneboardlist : "+oneboardlist);
@@ -257,17 +270,33 @@ public class CrewPageController {
 		public String crew_photo_write(MultipartFile file,@RequestParam String content,@RequestParam String board_id,@RequestParam int crew_idx) {
 				
 			logger.info("입력값 {} ",content);
-			if (!file.isEmpty()) {
-	            // 파일 처리 로직 (저장, DB에 저장 등)
-	            String fileName = file.getOriginalFilename();
-	            // 파일 저장하는 코드 추가
-	        }
-						
+			
 			crewpage_service.crew_photo_write(file,content,board_id,crew_idx);
 						
-			return "redirect:/crew_oneboard.go";
+			return "index";
 			
 		}
+		
+		// 사진 게시글 상세보기
+		@RequestMapping(value="/crew_photo_detail.go")
+		public String crew_photo_detail(String board_idx,Model model) {
+				
+			crewpage_service.crew_photo_detail(board_idx, model);
+
+			return "crew_photo_detail";
+			
+		}
+		
+		// 사진게시글 삭제
+				@GetMapping(value="/crew_photo_del")
+				public String crew_photo_del(@RequestParam String board_idx) {
+					
+					// logger.info("oneboard 보드번호는 ? " + board_idx);
+					
+					crewpage_service.crew_photo_del(board_idx);
+					
+					return "index";
+				}
 		
 		
 }
