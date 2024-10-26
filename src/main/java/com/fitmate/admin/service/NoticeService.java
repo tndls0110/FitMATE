2b73f5f699e584bed0e2cca0da773843da888e11
suin_laps <suin@email.com>
2024-10-26 19:15:18 +0900

    admin notice write 작성 완료

--- src/main/java/com/fitmate/admin/service/NoticeService.java
+++ src/main/java/com/fitmate/admin/service/NoticeService.java
@@ -1,11 +1,44 @@
 package com.fitmate.admin.service;
 
+import com.fitmate.admin.dao.NoticeDAO;
+import com.fitmate.admin.dto.NoticeDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class NoticeService {
 	Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired NoticeDAO notice_dao;
 
+	// 공지사항 목록
+	public Map<String, Object> list(int page, int cnt, String opt, String keyword) {
+		int limit = cnt;
+		int offset = (page - 1) * cnt;
+		int totalPages = notice_dao.allCount(cnt);
+		int totalIdx = notice_dao.countIdx();
+
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("totalPages", totalPages);
+		result.put("totalIdx", totalIdx);
+		result.put("currentPage", page);
+		result.put("offset", offset);
+		result.put("list", notice_dao.list(limit, offset, opt, keyword));
+
+		return result;
+	}
+
+	// 공지사항 작성
+	public boolean write(String notice_cont, int admin_idx) {
+		boolean result = false;
+		if (notice_dao.write(notice_cont, admin_idx) == 1){
+			result = true;
+		}
+		return result;
+	}
 }
