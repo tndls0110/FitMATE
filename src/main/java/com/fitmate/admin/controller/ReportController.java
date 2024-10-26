a4d920b83d038b88cd035fcf5bbe718411c9a6c0
suin_laps <suin@email.com>
2024-10-26 16:01:44 +0900

    admin report 데이터 부재로 인한 작업 중단

--- src/main/java/com/fitmate/admin/controller/ReportController.java
+++ src/main/java/com/fitmate/admin/controller/ReportController.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ReportController {
@@ -14,17 +17,23 @@ public class ReportController {
 
 	// 세션 체크
 	String page = "";
+	@Autowired MainController main_controller;
 
 	// 신고 목록
 	@RequestMapping (value = "/admin_reportList.go")
-	public String noticeList(){
+	public String noticeList(Model model, HttpSession session) {
 		page = "admin_reportList";
+		//main_controller.checkPermit(model, session);
+		model.addAttribute("list", report_service.getlist());
 		return page;
 	}
 
 	// 신고 상새보기
 	@RequestMapping (value = "/admin_reportDetail.go")
 	public String noticeDetail(){
+		//main_controller.checkPermit(model, session);
+		//int admin_idx = (int) session.getAttribute("loginIdx");
+		int admin_idx = 1;
 		page = "admin_reportDetail";
 		return page;
 	}
--- src/main/java/com/fitmate/admin/service/MainService.java
+++ src/main/java/com/fitmate/admin/service/MainService.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -35,4 +33,8 @@ public class MainService {
         }
         return result;
     }
+    public int getidx(String admin_id) {
+        return main_dao.getidx(admin_id);
+    }
+
 }
