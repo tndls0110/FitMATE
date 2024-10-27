f3f58c510b931df17a20770009afdde25e754115
류경민 <rrarro000115@gmail.com>
2024-10-27 19:45:55 +0900

    ScheduleController/Service/DAO 수정

--- src/main/java/com/fitmate/schedule/service/ScheduleService.java
+++ src/main/java/com/fitmate/schedule/service/ScheduleService.java
@@ -1,11 +1,20 @@
 package com.fitmate.schedule.service;
 
+import com.fitmate.schedule.dao.ScheduleDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ScheduleService {
+	@Autowired
+	ScheduleDAO s_dao = new scheduleDAO();
 	Logger logger = LoggerFactory.getLogger(getClass());
 
+	public Date getEvents() {
+		return s_dao.getEvents();
+	}
 }
