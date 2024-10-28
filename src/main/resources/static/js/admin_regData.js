f15da7ec2a5903d7ae266bedd930350e4d57bafa
suin_laps <suin@email.com>
2024-10-28 09:11:14 +0900

    24.10.28.

--- src/main/resources/static/js/admin_regData.js
+++ src/main/resources/static/js/admin_regData.js
@@ -1,16 +1,17 @@
+// 신고 사유
 // 수정
-function updateData(elem, idx) {
+function updateReport(elem, idx) {
     var contents = elem.value;
     modal.showConfirm(elem.value+" 항목으로 수정하시겠습니까?", "admin_updateReport.do");
 }
 
 // 삭제
-function deleteData(elem, idx) {
+function deleteReport(elem, idx) {
     modal.showConfirm(elem.value+" 항목을 삭제하시겠습니까?", "admin_deleteReport.do");
 }
 
 // 작성
-function insertData(){
+function insertReport(){
     var elem = document.getElementsByClassName('insertData')[0];
     modal.showConfirm(elem.value+" 항목을 작성하시겠습니까?", "admin_insertReport.do");
 }
