944f7df02277abc69f8ab6a231caa8322f9d2d65
류경민 <rrarro000115@gmail.com>
2024-10-28 09:09:01 +0900

    이벤트 있는 일정 가져오는 DAO 단 작성

--- src/main/java/com/fitmate/schedule/dao/ScheduleDAO.java
+++ src/main/java/com/fitmate/schedule/dao/ScheduleDAO.java
@@ -3,9 +3,11 @@ package com.fitmate.schedule.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ScheduleDAO {
+    List<Map<String,Object>> getEvents();
 
-    Date getEvents();
 }
