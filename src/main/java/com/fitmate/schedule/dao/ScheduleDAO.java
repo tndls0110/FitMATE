cad782615571750e6fdd5abd33130ee3308848fd
류경민 <rrarro000115@gmail.com>
2024-10-28 16:55:37 +0900

    개인 일지 가져오는 중...

--- src/main/java/com/fitmate/schedule/dao/ScheduleDAO.java
+++ src/main/java/com/fitmate/schedule/dao/ScheduleDAO.java
@@ -2,12 +2,11 @@ package com.fitmate.schedule.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface ScheduleDAO {
     List<Map<String,Object>> getEvents();
-    List<Map<String, Object>> getJournal(String date);
+    List<Map<String, Object>> getJournal(String date, String id);
 }
--- src/main/java/com/fitmate/schedule/service/ScheduleService.java
+++ src/main/java/com/fitmate/schedule/service/ScheduleService.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +18,8 @@ public class ScheduleService {
 		return s_dao.getEvents();
 	}
 
-	public List<Map<String, Object>> getJournal(String date) {
-		List<Map<String,Object>> journal_list = s_dao.getJournal(date);
+	public List<Map<String, Object>> getJournal(String date, String id) {
+		List<Map<String,Object>> journal_list = s_dao.getJournal(date,id);
+		return journal_list;
 	}
 }
