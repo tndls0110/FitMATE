e8ba09066c24c894786bcbe8ab0592b0e42521b2
류경민 <rrarro000115@gmail.com>
2024-11-03 19:11:45 +0900

    알림 기능 작성

--- src/main/java/com/fitmate/notify/controller/NotiController.java
+++ src/main/java/com/fitmate/notify/controller/NotiController.java
@@ -1,11 +1,61 @@
 package com.fitmate.notify.controller;
 
+import com.fitmate.notify.service.NotiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class NotiController {
 	Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired NotiService n_Service;
+	@RequestMapping (value = "/alarm.go")
+	public String goAlarm () {
+		return "alarm";
+	}
 
+	@GetMapping (value = "/get_all_alarm.ajax")
+	@ResponseBody
+	public Map<String, Object> getAllAlarm (int idx) {
+		logger.info("idx:{}",idx);
+		Map<String, Object> data = new HashMap<String, Object>();
+		String id = "member01";
+
+		data.put("id", n_Service.getAllAlarm(id,idx));
+		return data;
+	}
+
+	@GetMapping (value = "/read_change.ajax")
+	@ResponseBody
+	public Map<String, Object> read_change (int idx) {
+		logger.info("idx:{}",idx);
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("success", n_Service.read_change(idx));
+		return data;
+	}
+
+
+	@GetMapping (value = "/delete_alarm.ajax")
+	@ResponseBody
+	public Map<String, Object> deleteAlarm (int idx) {
+		logger.info("idx:{}",idx);
+		Map<String, Object> data = new HashMap<>();
+		data.put("success", n_Service.deleteAlarm(idx));
+		return data;
+	}
+
+	@GetMapping (value = "/check_unreadAlarm.ajax")
+	@ResponseBody
+	public Map<String, Object> checkUnreadAlarm () {
+		String id = "member01";
+		Map<String, Object> data = new HashMap<>();
+		String exists = n_Service.checkUnreadAlarm(id);
+	}
 }
--- src/main/java/com/fitmate/notify/service/NotiService.java
+++ src/main/java/com/fitmate/notify/service/NotiService.java
@@ -1,11 +1,55 @@
 package com.fitmate.notify.service;
 
+import com.fitmate.notify.dao.NotiDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NotiService {
 	Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired NotiDAO n_DAO;
+    public List<NotiDAO> getAllAlarm(String id, int idx) {
+        logger.info("service에서 받은 idx:{}",idx);
+        List<NotiDAO> list = new ArrayList<>();
+        if(idx == 0){
+            list = n_DAO.getAllAlarm(id);
+        }else{
+            list = n_DAO.getspecificAlarm(id,idx);
+        }
+    return list;
+    }
 
+    public boolean read_change(int idx) {
+        boolean success =false;
+        int updated_rows = n_DAO.read_change(idx);
+        if(updated_rows>0){
+            success = true;
+        }
+        return success;
+    }
+
+    public Object deleteAlarm(int idx) {
+        int updated_rows = n_DAO.deleteAlarm(idx);
+        boolean success = false;
+        if(updated_rows>0){
+            success = true;
+        }
+        return success;
+    }
+
+    public String checkUnreadAlarm(String id) {
+        logger.info("서비스에서 전달받은 id :{}",id);
+        String result = "none";
+        int exist = n_DAO.checkUnreadAlarm(id);
+        if(exist>0){
+            result ="exist";
+        }
+
+        return result;
+    }
 }
