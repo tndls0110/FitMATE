6733def2830e14f77e385076b09ffe5a95bd957a
suin <tndls@email.net>
2024-11-05 16:27:33 +0900

    admin 세션체크 기능 수정

--- src/main/java/com/fitmate/admin/controller/MainController.java
+++ src/main/java/com/fitmate/admin/controller/MainController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
